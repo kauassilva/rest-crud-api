c254686af2119380e5444dae4987e820fd3b6e9f
Kaua Silva <kauassilva1@gmail.com>
2024-02-22 19:58:42 -0300

    Added getStudent by id method in StudentRestController

--- demo/src/main/java/com/learn/demo/rest/StudentRestController.java
+++ demo/src/main/java/com/learn/demo/rest/StudentRestController.java
@@ -3,6 +3,7 @@ package com.learn.demo.rest;
 import com.learn.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +30,12 @@ public class StudentRestController {
     @GetMapping("/students")
     public List<Student> getStudents() {
         return theStudents;
+    }
+
+    // Define endpoint for "/students/{studentId}" - return student at index
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return theStudents.get(studentId);
     }
 
 }
