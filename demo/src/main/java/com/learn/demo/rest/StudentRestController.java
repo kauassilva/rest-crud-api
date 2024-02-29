f5ca909fd4da1eeb81ee12b520f7cf26bea4d463
Kaua Silva <kauassilva1@gmail.com>
2024-02-29 18:49:19 -0300

    Created ControllerAdvice class for global exception handling

--- demo/src/main/java/com/learn/demo/rest/StudentRestController.java
+++ demo/src/main/java/com/learn/demo/rest/StudentRestController.java
@@ -2,10 +2,10 @@ package com.learn.demo.rest;
 
 import com.learn.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.apache.coyote.Response;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,7 +26,7 @@ public class StudentRestController {
         theStudents.add(new Student("Mary", "Smith"));
     }
 
-    // Define endpoint for "/students"  - return a list of students
+    // Define endpoint for "/students" - return a list of students
     @GetMapping("/students")
     public List<Student> getStudents() {
         return theStudents;
@@ -41,34 +41,6 @@ public class StudentRestController {
         }
 
         return theStudents.get(studentId);
-    }
-
-    // Add an exception handler using @ExceptionHandler
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
-        // Create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // Return ResponseEntity
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    // Add another exception handler ... to catch any exception (catch all)
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
-        // Create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // Return ResponseEntity
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
 }
